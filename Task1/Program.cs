2923b646800de84a5ac13de6bf0cd2232c83e22c
lena94434 <lena94434@gmail.com>
2019-03-30 14:50:50 +0200

    First

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,7 +10,16 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            int num1 = 10;
+            int num2 = 15;
+
+            Console.WriteLine($"{num1}+{num2}={Program.Sum(num1,num2)}");
+            Console.ReadLine();
+        }
+
+        public static int Sum(int x, int y)
+        {
+            return x + y;
         }
     }
 }
