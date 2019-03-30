02fc9d3ea7368fa6114553234a59b5588e550d51
lena94434 <lena94434@gmail.com>
2019-03-30 15:23:30 +0200

    Average

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,6 +8,8 @@ namespace Task1
 {
     class Program
     {
+        public static object Converte { get; private set; }
+
         static void Main(string[] args)
         {
             long num1 = 10;
@@ -31,12 +33,34 @@ namespace Task1
             //
 
             Console.WriteLine((int)dec);
+
+            Console.WriteLine("Enter 3 numbers");
+
+            string n1 = Console.ReadLine();
+            string n2 = Console.ReadLine();
+            string n3 = Console.ReadLine();
+
+            int.TryParse(n1, out int number1);
+            int.TryParse(n2, out int number2);
+            int.TryParse(n3, out int number3);
+
+            decimal average = AverageMean(number1, number2, number3);
+
+            Console.WriteLine($" Среднее значение = {average}");
+
             Console.ReadKey();
+
+
         }
 
         public static int Sum(int x, int y)
         {
             return x + y;
+        }
+
+        public static decimal AverageMean(int a,int b, int c)
+        {
+            return (decimal)(a + b + c)/ 3;
         }
 
 
