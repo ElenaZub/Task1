c8f94a08c2e84fa6490e492685ba8d410c9fee0e
lena94434 <lena94434@gmail.com>
2019-03-30 14:51:57 +0200

    Second

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,7 +10,7 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            int num1 = 10;
+            short num1 = 10;
             int num2 = 15;
 
             Console.WriteLine($"{num1}+{num2}={Program.Sum(num1,num2)}");
