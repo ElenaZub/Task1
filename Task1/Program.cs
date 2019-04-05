f87911b64bfb754b3db4c667ad17cd2eeb0fd821
lena94434 <lena94434@gmail.com>
2019-04-05 21:30:57 +0300

    Задание №2. Переменные

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -11,12 +11,22 @@ namespace Task1
         static void Main(string[] args)
         {
             Task1();
+            Task2();
         }
 
         static public void Task1()
         {
             int x1 = 6;
             Console.WriteLine(x1);
+        }
+
+        static public void Task2()
+        {
+            string str1;
+            Console.WriteLine("Ваше имя?");
+            str1 = Console.ReadLine();
+            string str2 = "Привет " + str1;
+            Console.WriteLine(str2);
         }
     }
 }
