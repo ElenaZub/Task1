162c032b355188973a930f883d9239b2d66df6bc
ElenaZub <zublena@bk.ru>
2019-05-26 17:27:27 +0300

    Make first letter of each word uppercase

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,7 +10,27 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            
+            ConverMessage(EnterMessage());
+
+            Console.ReadKey();
+        }
+
+        public static void ConverMessage(string msg)
+        {
+            string[] arrayStr = msg.Split(' ');
+            string arrayWithUpp = String.Empty;
+            for (int i = 0; i < arrayStr.Length; i++)
+            {
+                arrayWithUpp += arrayStr[i].Substring(0, 1).ToUpper() + arrayStr[i].Substring(1) + " ";
+            }
+
+            Console.WriteLine(arrayWithUpp);
+        }
+
+        public static string EnterMessage()
+        {
+            Console.WriteLine("Enter message:");
+            return Console.ReadLine();
         }
     }
 }
