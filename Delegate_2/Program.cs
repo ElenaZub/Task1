79cb23e01715e4ebfb29faab91a6f56d7e131b90
lena94434 <lena94434@gmail.com>
2019-06-28 11:41:25 +0300

    .

---
+++ Delegate_2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_2
+{
+    class Program
+    {
+        public delegate void MyDelegate();
+
+        public static void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+
+        public static void Method2()
+        {
+            Console.WriteLine("Method2");
+        }
+
+        public static void Method3()
+        {
+            Console.WriteLine("Method3");
+        }
+
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = null;
+            MyDelegate myDelegate1 = new MyDelegate(Method1);
+            MyDelegate myDelegate2 = new MyDelegate(Method2);
+            MyDelegate myDelegate3 = new MyDelegate(Method3);
+
+            myDelegate = myDelegate1 + myDelegate2 + myDelegate3;
+
+            bool check = true;
+
+            while (check)
+            {
+                Console.WriteLine("Enter number from 1 to 7");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        {
+                            myDelegate1.Invoke();
+                            break;
+                        }
+                    case "2":
+                        {
+                            myDelegate2.Invoke();
+                            break;
+                        }
+                    case "3":
+                        {
+                            myDelegate3.Invoke();
+                            break;
+                        }
+                    case "4":
+                        {
+                            MyDelegate myDelegate4 = myDelegate - myDelegate1;
+                            myDelegate4.Invoke();
+                            break;
+                        }
+                    case "5":
+                        {
+                            MyDelegate myDelegate5 = myDelegate - myDelegate2;
+                            myDelegate5.Invoke();
+                            break;
+                        }
+                    case "6":
+                        {
+                            MyDelegate myDelegate6 = myDelegate - myDelegate3;
+                            myDelegate6.Invoke();
+                            break;
+                        }
+                    case "7":
+                        {
+                            (myDelegate1 + myDelegate2).Invoke();
+                            break;
+                        }
+                    case "0":
+                        {
+                            check = false;
+                            break;
+                        }
+                    default:
+                        Console.WriteLine("You enter incorrect value");
+                        break;
+                }
+            }
+           
+        }
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,11 +6,56 @@ using System.Threading.Tasks;
 
 namespace Task1
 {
+
+    public delegate double Calculate(double firstArgument, double secondArgument);
+
+
+    public class Helper
+    {
+        public void Execute(Calculate del)
+        {
+            double firstArgument = 10;
+            double secondArgument = 20;
+
+            var calculationResult = del.Invoke(firstArgument, secondArgument);
+
+            Console.WriteLine(calculationResult);
+        }
+    }
+
+    class Calculator
+    {
+
+        public double Multyply(double firstArgument, double secondArgument)
+        {
+            return firstArgument * secondArgument;
+        }
+
+        public double Sum(double firstArgument, double secondArgument)
+        {
+            return firstArgument + secondArgument;
+        }
+
+        public double Divide(double firstArgument, double secondArgument)
+        {
+            return firstArgument / secondArgument;
+        }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Helper help = new Helper();
+            Calculator calc = new Calculator();
+
+            help.Execute(calc.Divide);
+            help.Execute(calc.Sum);
+            help.Execute(calc.Multyply);
+
+            Console.ReadKey();
         }
     }
 }
