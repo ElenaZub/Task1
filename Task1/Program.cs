8205eedbcd3165244437ee48c2266c82114f1a17
ElenaZub <zublena@bk.ru>
2019-06-29 12:37:35 +0300

    Work with class and objects

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,7 +10,13 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            
+            Worker firstWorker = new Worker("Ivan", 25, 1000);
+            Worker secondWorker = new Worker("Vasya", -3, 2000);
+
+            Console.WriteLine($"Sum of salary {firstWorker.Salary + secondWorker.Salary}");
+            Console.WriteLine($"Sum of ages {firstWorker.Age + secondWorker.Age}");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Task1/Worker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Worker
+    {
+
+        private int age = 1;
+
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value >= 1 && value <= 100)
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public Worker(string name, int age, decimal salary)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Salary = salary;
+        }
+    }
+}
