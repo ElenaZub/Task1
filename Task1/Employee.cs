2b9dc6724faa8f033d5e340fee983f91fbe6da11
lena94434 <lena94434@gmail.com>
2019-07-07 12:13:58 +0300

    Defered and Undefered operators.

---
+++ Task1/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Employee
+    {
+        public string FirstName{ get; set; }
+
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+        public Employee(string firstName, string lastName, string gender)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Gender = gender;
+        }
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,7 +10,48 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            
+            UnDeferedExemple();
+            //DeferedExemple();
+        }
+
+        public static void DeferedExemple()
+        {
+            var employees = new List<Employee>()
+            {
+                new Employee("Elena", "Zub", "F"),
+                new Employee("Ivan", "Ivanova", "M"),
+                new Employee("Yana", "Vasyna", "F"),
+                new Employee("Petr", "Petrov", "F")
+            };
+            employees.ToList();
+            var maleEmployees = employees.Where(employee => employee.Gender == "M");
+            Display(maleEmployees);
+            employees.Add(new Employee("Sasha", "Levchenko", "M"));
+            Display(maleEmployees);
+        }
+
+        public static void UnDeferedExemple()
+        {
+            var employees = new List<Employee>()
+            {
+                new Employee("Elena", "Zub", "F"),
+                new Employee("Ivan", "Ivanova", "M"),
+                new Employee("Yana", "Vasyna", "F"),
+                new Employee("Petr", "Petrov", "F")
+            };
+            var maleEmployees = employees.Where(employee => employee.Gender == "M").ToList();
+            Display(maleEmployees);
+            employees.Add(new Employee("Sasha", "Levchenko", "M"));
+            Display(maleEmployees);
+        }
+
+        public static void Display(IEnumerable<Employee> employees)
+        {
+            foreach (var item in employees)
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName}");
+            }
+            Console.WriteLine(new String('-', 30));
         }
     }
 }
