e8648238d928f1513e8aebfb1673a9ba11490913
lena94434 <lena94434@gmail.com>
2019-07-16 17:19:29 +0300

    Max, min, mean, sum, odd elements.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,7 +10,90 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter amount of numbers in array:");
+            int n = int.Parse(Console.ReadLine());
+
+            int [] array = CreateRandomArray(n);
+            PrintArray(array);
+
+            Min(array);
+            Max(array);
+            Mean(array);
+            Odd(array);
+        }
+
+        public static int[] CreateRandomArray(int length)
+        {
+            Random rand = new Random();
+            int[] array = new int[length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(1, 100);
+            }
+            return array;
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }  
+            Console.WriteLine();
+        }
+
+        public static void Min(int[] array)
+        {
+            int min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            Console.WriteLine($"Min value is {min}");
+        }
+
+        public static void Max(int[] array)
+        {
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+
+            Console.WriteLine($"Max value is {max}");
+        }
+
+        public static void Mean(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }   
+            double mean = (double)sum / array.Length;
+
+            Console.WriteLine($"Sum of elements is {sum}");
+            Console.WriteLine($"Mean value is {mean}");
+        }
+
+        public static void Odd(int[] array)
+        {
+            Console.WriteLine("Odd elements:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 1)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
         }
     }
 }
