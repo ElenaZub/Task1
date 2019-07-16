b2ed93a9ca53385c331b72a7496f4f0ae4c0bd8d
lena94434 <lena94434@gmail.com>
2019-07-16 17:29:50 +0300

    Indexes of elements in range.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -16,10 +16,11 @@ namespace Task1
             int [] array = CreateRandomArray(n);
             PrintArray(array);
 
-            Min(array);
-            Max(array);
-            Mean(array);
-            Odd(array);
+            //Min(array);
+            //Max(array);
+            //Mean(array);
+            //Odd(array);
+            IndexesOfElementsInRange(array);
         }
 
         public static int[] CreateRandomArray(int length)
@@ -93,6 +94,22 @@ namespace Task1
                 {
                     Console.WriteLine(array[i]);
                 }
+            }
+        }
+
+        public static void IndexesOfElementsInRange(int[] array)
+        {
+            Console.WriteLine("Enter min value:");
+            int min = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter max value:");
+            int max = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Indexes of elements that are more then {min} and less then {max}:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] > min && array[i] < max)
+                    Console.WriteLine(i);
             }
         }
     }
