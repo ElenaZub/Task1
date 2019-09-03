31e79b1de2d9b17f5d8441e9bf88b832c38fcf22
lena94434 <lena94434@gmail.com>
2019-09-03 19:26:03 +0300

    not final

--- Task1/ListManager.cs
+++ Task1/ListManager.cs
@@ -9,19 +9,50 @@ namespace Task1
     class ListManager
     {
         public void Execute()
-        {           
+        {
+            MyList<int> list = new MyList<int>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
 
+            list.Add(1);
+            list.Add(5);
+            list.Add(8);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            if (list.Contains(1))
+            {
+                Console.WriteLine("This item is contained in array");
+            }
+            else
+            {
+                Console.WriteLine("This item is NOT contained in array");
+            }
+
+            list.Clear();
+            Console.WriteLine($"Count after clearing: {list.Count}");
         }
 
-        public void CreateList()
+        public MyList<int> CreateList()
         {
-            IMyList<int> myList = new MyList<int>();
+            MyList<int> myList = new MyList<int>();
             Random rand = new Random();
 
             for (int i = 0; i < 10; i++)
             {
                 myList.Add(rand.Next(100));
             }
+            return myList;
+        }
+
+        public override string ToString()
+        {
+            return $"";
         }
     }
 }
